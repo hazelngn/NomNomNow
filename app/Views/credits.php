abd74a185d0213b63a3c65919352d2d95119b9d2
Hazel <p.a.nguyen.working@gmail.com>
2024-04-08 00:48:16 +0000

    adding table num to menu order system

---
+++ app/Views/credits.php
@@ -0,0 +1,16 @@
+<?= $this->extend('template') ?>
+<?= $this->section('content') ?>
+    <h3>Credits</h3>
+    <section>
+        <a href="https://www.flaticon.com/free-icons/menu" title="menu icons">Menu icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/project-management" title="project management icons">Project management icons created by itim2101 - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/qr-code" title="qr code icons">Qr code icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/salad" title="salad icons">Salad icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/starter" title="starter icons">Starter icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/dinner" title="dinner icons">Dinner icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/dessert" title="dessert icons">Dessert icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/milk-tea" title="milk tea icons">Milk tea icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/alcohol" title="alcohol icons">Alcohol icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/soda" title="soda icons">Soda icons created by Chanut-is-Industries - Flaticon</a>
+    </section>
+<?= $this->endSection() ?>
--- app/Views/templates/components/order_item_card.php
+++ app/Views/templates/components/order_item_card.php
@@ -1,14 +1,14 @@
-<section class="indicator w-full flex flex-row p-3 mb-5 md:p-2 rounded-3xl bg-neutral items-center md:flex-col md:basis-5/12 md:grow-0 lg:basis-4/12">
+<section class="indicator w-full flex flex-row p-5 mb-5 md:p-5 rounded-3xl bg-neutral items-center md:flex-col md:basis-5/12 md:grow-0 lg:basis-3/12">
     <span class="indicator-item badge badge-secondary badge-sm">Note</span> 
-    <section class="flex flex-col gap-3 w-10/12 md:text-lg md:w-8/12" onclick="status_md.showModal()">
-        <h2 class="flex justify-between font-bold text-md md:mt-5">
+    <section class="flex flex-col gap-3 w-10/12 md:w-full md:text-lg" onclick="status_md.showModal()">
+        <h2 class="flex justify-between font-bold text-md">
             Greek Salad
         </h2>
         <div class="flex flex-row justify-between">
-            <p>$10</p>
+            <p>Table: 1</p>
         </div>
     </section>
-    <div class="flex flex-row gap-2 flex-wrap">
+    <div class="flex flex-row gap-2 md:w-full md:mt-3 flex-wrap">
         <div class="badge badge-accent badge-outline shrink-0 md:text-md md:p-3 w-max">Diet 1</div>
         <div class="badge badge-accent badge-outline shrink-0 md:text-md md:p-3 w-max">Diet 2</div>
     </div>
